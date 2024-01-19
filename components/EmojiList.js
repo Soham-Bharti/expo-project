bda981dfdb597ab774f9ae5f5ab718945837d4a4
= <sohamom7@gmail.com>
2024-01-19 01:05:28 -0800

    tap feature + ...upto dragging

---
+++ components/EmojiList.js
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import { StyleSheet, FlatList, Image, Platform, Pressable } from 'react-native';
+
+export default function EmojiList({ onSelect, onCloseModal }) {
+  const [emoji] = useState([
+    require('../assets/images/emoji1.jpg'),
+    require('../assets/images/emoji2.jpg'),
+    require('../assets/images/emoji3.jpg'),
+    require('../assets/images/emoji4.jpg'),
+    require('../assets/images/emoji5.jpg'),
+    require('../assets/images/emoji6.jpg'),
+    require('../assets/images/emoji7.jpg'),
+    require('../assets/images/emoji8.jpg'),
+    require('../assets/images/emoji9.jpg'),
+    require('../assets/images/emoji10.jpg'),
+  ]);
+
+  return (
+    <FlatList
+      horizontal
+      showsHorizontalScrollIndicator={Platform.OS === 'web'}
+      data={emoji}
+      contentContainerStyle={styles.listContainer}
+      renderItem={({ item, index }) => (
+        <Pressable
+          onPress={() => {
+            onSelect(item);
+            onCloseModal();
+          }}>
+          <Image source={item} key={index} style={styles.image} />
+        </Pressable>
+      )}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  listContainer: {
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginRight: 20,
+  },
+});
